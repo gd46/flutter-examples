d706e292bdcd1e3cc381b311fc33ec4d2c311020
Giuseppe DiBella <gdibella21@gmail.com>
2022-02-27 22:07:17 -0500

    add example of changing default back button behavior to look at webview history

--- lib/pages/webview/webview_communication_example_page.dart
+++ lib/pages/webview/webview_communication_example_page.dart
@@ -30,82 +30,93 @@ class _WebViewCommunicationExamplePageState
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Webview Communication Example'),
-        actions: [
-          PopupMenuButton(
-              onSelected: (value) {
-                switch (value) {
-                  case MenuOptions.setTitle:
-                    _controller
-                        .runJavascript('Container.setTitle("From Flutter")');
-                    break;
-                  case MenuOptions.openDialog:
-                    _controller.runJavascript('Container.openDialog()');
-                    break;
-                  case MenuOptions.sendJson:
-                    _controller
-                        .runJavascript('Container.sendJson({"title": "Test"})');
-                    break;
-                }
-              },
-              itemBuilder: (context) => [
-                    const PopupMenuItem(
-                        value: MenuOptions.setTitle, child: Text('Set Title')),
-                    const PopupMenuItem(
-                        value: MenuOptions.openDialog,
-                        child: Text('Open Dialog')),
-                    const PopupMenuItem(
-                        value: MenuOptions.sendJson, child: Text('Send Json'))
-                  ])
-        ],
-      ),
-      body: IndexedStack(
-        index: loadingState.index,
-        children: [
-          const Center(
-            child: CircularProgressIndicator(),
-          ),
-          WebView(
-              initialUrl: 'http://localhost:4200/',
-              javascriptMode: JavascriptMode.unrestricted,
-              javascriptChannels: {
-                JavascriptChannel(
-                    name: 'snackBarHandler',
-                    onMessageReceived: (JavascriptMessage message) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text(message.message)));
-                    }),
-                JavascriptChannel(
-                    name: 'dialogHandler',
-                    onMessageReceived:
-                        (JavascriptMessage javascriptMessage) async {
-                      final dialogConfig =
-                          jsonDecode(javascriptMessage.message);
-                      await showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: Text(dialogConfig['title']),
-                            );
-                          });
-                    })
-              },
-              onPageStarted: (value) {
-                setState(() {
-                  loadingState = LOADING_STATE.loading;
-                });
-              },
-              onPageFinished: (value) {
-                setState(() {
-                  loadingState = LOADING_STATE.loadingCompleted;
-                });
-              },
-              onWebViewCreated: (WebViewController webviewController) {
-                _controller = webviewController;
-              }),
-        ],
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _controller.canGoBack()) {
+          _controller.goBack();
+          return false;
+        }
+
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Webview Communication Example'),
+          actions: [
+            PopupMenuButton(
+                onSelected: (value) {
+                  switch (value) {
+                    case MenuOptions.setTitle:
+                      _controller
+                          .runJavascript('Container.setTitle("From Flutter")');
+                      break;
+                    case MenuOptions.openDialog:
+                      _controller.runJavascript('Container.openDialog()');
+                      break;
+                    case MenuOptions.sendJson:
+                      _controller.runJavascript(
+                          'Container.sendJson({"title": "Test"})');
+                      break;
+                  }
+                },
+                itemBuilder: (context) => [
+                      const PopupMenuItem(
+                          value: MenuOptions.setTitle,
+                          child: Text('Set Title')),
+                      const PopupMenuItem(
+                          value: MenuOptions.openDialog,
+                          child: Text('Open Dialog')),
+                      const PopupMenuItem(
+                          value: MenuOptions.sendJson, child: Text('Send Json'))
+                    ])
+          ],
+        ),
+        body: IndexedStack(
+          index: loadingState.index,
+          children: [
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+            WebView(
+                initialUrl: 'http://localhost:4200/',
+                javascriptMode: JavascriptMode.unrestricted,
+                javascriptChannels: {
+                  JavascriptChannel(
+                      name: 'snackBarHandler',
+                      onMessageReceived: (JavascriptMessage message) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(message.message)));
+                      }),
+                  JavascriptChannel(
+                      name: 'dialogHandler',
+                      onMessageReceived:
+                          (JavascriptMessage javascriptMessage) async {
+                        final dialogConfig =
+                            jsonDecode(javascriptMessage.message);
+                        await showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text(dialogConfig['title']),
+                              );
+                            });
+                      })
+                },
+                onPageStarted: (value) {
+                  setState(() {
+                    loadingState = LOADING_STATE.loading;
+                  });
+                },
+                onPageFinished: (value) {
+                  setState(() {
+                    loadingState = LOADING_STATE.loadingCompleted;
+                  });
+                },
+                onWebViewCreated: (WebViewController webviewController) {
+                  _controller = webviewController;
+                }),
+          ],
+        ),
       ),
     );
   }
